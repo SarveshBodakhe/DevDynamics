3802f1e79038bb1eebb88a07badae67e8525bc79
Sarvesh Bodakhe <sarveshbodakhe77@gmail.com>
2024-06-08 17:36:23 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+
+app.get('/api/data', (req, res) => {
+  res.sendFile(__dirname + '/data.json');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
